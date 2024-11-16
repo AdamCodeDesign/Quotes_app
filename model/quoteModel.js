06c31ee061a18de180a5c643a89958b3ff58dfdf
LeshchuDev <leshchudev@gmail.com>
2024-11-16 11:57:58 +0100

    Model created- saveAll, getAll and getById functions

---
+++ model/quoteModel.js
@@ -0,0 +1,42 @@
+import MongoSingleton from "../data/mongoDbSIngleton";
+import { ObjectId } from "mongodb";
+
+export function saveAll(quotes) {
+  return new Promise(async (resolve, reject) => {
+    const collection = await MongoSingleton.getCollection();
+    const result = await collection.insertMany(quotes);
+
+    if (result.insertedCount) {
+      resolve(result);
+    } else {
+      reject("Could not save quotes");
+    }
+  });
+}
+
+export function getAll() {
+  return new Promise(async (resolve, reject) => {
+    const collection = await MongoSingleton.getCollection();
+    const cursor = await collection.find();
+    const results = await cursor.toArray();
+
+    if (results.length > 0) {
+      resolve(results);
+    } else {
+      reject("Could not get all quotes");
+    }
+  });
+}
+
+export function getById(id) {
+  return new Promise(async (resolve, reject) => {
+    const collection = await MongoSingleton.getCollection();
+    const result = await collection.findOne({ _id: ObjectId(id) });
+
+    if (result) {
+      resolve(result);
+    } else {
+      reject("Could not get quote by Id:" + id);
+    }
+  });
+}
