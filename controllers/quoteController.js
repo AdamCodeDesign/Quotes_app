5b7a931bfa3df5eaac96d003bf5dacabde7cfcba
LeshchuDev <leshchudev@gmail.com>
2024-11-16 12:33:47 +0100

    controller finished - getQuote, getQuotes, getRandom, prepareDB created

---
+++ controllers/quoteController.js
@@ -0,0 +1,94 @@
+import { getAll, getById, saveAll } from "../model/quoteModel";
+
+export async function getQuotes() {
+  try {
+    const quotes = await getAll();
+    return quotes;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export async function getQuote(id) {
+  try {
+    const quote = await getById(id);
+    return quote;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export async function getRandom() {
+  try {
+    const quotes = await getQuotes();
+
+    if (quotes) {
+      return quotes[Math.floor(Math.random() * quotes.length)];
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export async function prepareDB() {
+  try {
+    const quotes = await getQuotes();
+
+    if (!quotes) {
+      const quotesArr = [
+        {
+          quote: "Be the change that you wish to see in the world.",
+          author: "Mahatma Gandhi",
+        },
+        {
+          quote:
+            "The only limit to our realization of tomorrow is our doubts of today.",
+          author: "Franklin D. Roosevelt",
+        },
+        {
+          quote:
+            "In the end, we will remember not the words of our enemies, but the silence of our friends.",
+          author: "Martin Luther King Jr.",
+        },
+        {
+          quote:
+            "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+          author: "Ralph Waldo Emerson",
+        },
+        {
+          quote:
+            "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+          author: "Winston Churchill",
+        },
+        {
+          quote: "You miss 100% of the shots you don’t take.",
+          author: "Wayne Gretzky",
+        },
+        {
+          quote:
+            "It does not matter how slowly you go as long as you do not stop.",
+          author: "Confucius",
+        },
+        {
+          quote: "The only thing we have to fear is fear itself.",
+          author: "Franklin D. Roosevelt",
+        },
+        {
+          quote: "Life is what happens when you’re busy making other plans.",
+          author: "John Lennon",
+        },
+      ];
+      await saveAll(quotesArr);
+    } else {
+      return quotes;
+    }
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
