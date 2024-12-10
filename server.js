1170ce770c29c7a79f5a614ffc1e5b468a2cb1d0
LeshchuDev <leshchudev@gmail.com>
2024-12-10 18:43:45 +0100

    server updated with POST - delete and update quotes

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ import {
   getQuotes,
   getRandom,
   prepareDB,
+  insertQuote,
+  deleteQuoteById,
+  updateQuoteById,
 } from "./controllers/quoteController.js";
 import { serverStaticFile } from "./util/staticServer.js";
 
 const PORT = 8080;
 const APP_CONTENT_TYPE = { "Content-Type": "application/json" };
+
 const server = http.createServer(async function (req, res) {
   console.log("Request");
 
@@ -48,6 +52,57 @@ const server = http.createServer(async function (req, res) {
     }
 
     res.end(JSON.stringify(quote));
+  } else if (req.url === "/api/quotes/save" && req.method === "POST") {
+    let data = "";
+
+    req.on("data", function (chunk) {
+      data += chunk;
+    });
+
+    req.on("end", async function () {
+      const quote = JSON.parse(data);
+
+      let response = {};
+      const result = await insertOne(quote);
+
+      if (result) {
+        res.writeHead(200);
+        response = { saved: true, _id: result.insertedId };
+      } else {
+        res.writeHead(404);
+        response = { saved: false, _id: null };
+      }
+
+      res.end(JSON.stringify(response));
+    });
+  } else if (req.url === "/api/quotes/delete" && req.method === "POST") {
+    let data = "";
+
+    req.on("data", function (chunk) {
+      data += chunk;
+    });
+
+    req.on("end", async function () {
+      const quote = JSON.parse(data);
+
+      if (!quote || !quote._id) {
+        res.end(JSON.stringify({ message: "Wrong id" }));
+        return;
+      }
+
+      let response = {};
+      const result = await deleteQuoteById(quote._id);
+
+      if (result && result.deletedCount > 0) {
+        res.writeHead(200);
+        response = { deleted: true };
+      } else {
+        res.writeHead(404);
+        response = { deleted: false };
+      }
+
+      res.end(JSON.stringify(response));
+    });
   } else {
     serverStaticFile(req, res);
   }
